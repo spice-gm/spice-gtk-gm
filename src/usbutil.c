spice-gm/spice-gtk-gm

commit 032ca202f839fe1c49cddfd2b0459f9fecc23c86

spice_usbutil_parse_usbids: verify at least one vendor and product
Fixes the following clang warning:
  src/usbutil.c:148:52: warning: Use of zero-allocated memory
  ...

   product_info[product_count].product_id = id;
                                          ^
   146|               while (isspace(line[0]))
   147|                   line++;
   148|->             product_info[product_count].product_id = id;
   149|               snprintf(product_info[product_count].name,
   150|                        PRODUCT_NAME_LEN, "%s", line);

Signed-off-by: Uri Lublin <uril@redhat.com>

src/usbutil.c
@@ -113,6 +113,13 @@ static gboolean spice_usbutil_parse_usbids(gchar *path)
         usbids_vendor_count++;
     }
 
+    if (usbids_vendor_info == 0 || product_count == 0) {
+        usbids_vendor_count = -1;
+        g_strfreev(lines);
+        g_free(contents);
+        return FALSE;
+    }
+
     usbids_vendor_info = g_new(usb_vendor_info, usbids_vendor_count);
     product_info = g_new(usb_product_info, product_count);
 

commit 2fca24cc239237c3d1dc82d381eb67df413ed049

Fix parsing the usbids file
This fixes a regression from 032ca202f839fe1c49cddfd2b0459f9fecc23c86
where a check on whether the usbids file contained anything but the
wrong variable was checked. This is visible on windows when trying to
share a USB device as devices won't get named based on the usbids file.
Same thing on linux if we can't read the name from the sysfs.

`usbids_vendor_info` is only initialized to something not NULL after it
was checked to be different from NULL which is obviously wrong. The
patch wanted to check that the `usbids_vendor_count` was different from
0 instead.

Acked-by: Frediano Ziglio <freddy77@gmail.com>

src/usbutil.c
@@ -113,7 +113,7 @@ static gboolean spice_usbutil_parse_usbids(gchar *path)
         usbids_vendor_count++;
     }
 
-    if (usbids_vendor_info == 0 || product_count == 0) {
+    if (usbids_vendor_count == 0 || product_count == 0) {
         usbids_vendor_count = -1;
         g_strfreev(lines);
         g_free(contents);